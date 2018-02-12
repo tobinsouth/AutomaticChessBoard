4eb71a5030d428c06345a5ecca14590329c11530
tmsouth <tobin.south@gmail.com>
2018-02-13 00:17:03 +1030

    removing old code

--- Not in use/Code (Deprecated)/5. AI vs AI/Chess_Knight.py
+++
@@ -1,6 +0,0 @@
-import numpy
-import Chess_Pathfinding as pathfinding
-import Chess_Serial as serial
-
-def knightMove(nmap,startPos,endPos,dist):
-	pass
--- Not in use/Code (Deprecated)/8. CV Working (Pre Ingenuity) - 24-10-17/crashTesting.py
+++
@@ -1,9 +0,0 @@
-import Computer_Vision as cv
-from time import sleep
-
-[x, y, w, h] = cv.setup()
-
-for x in xrange(1,10):
-	sleep(40)
-	Out = cv.openCV(x, y, w, h)
-	print Out
